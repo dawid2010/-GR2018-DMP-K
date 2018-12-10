3cb93c3c778a506868255ec938d899deac5b29a9
Wiklos <wiklos24@interia.eu>
2018-12-10 22:02:44 +0100

    Interfejs dodawania przychodów

---
+++ app/src/main/java/pawel/wiklo/swinkaskarbonka/addIncome.java
@@ -0,0 +1,13 @@
+package pawel.wiklo.swinkaskarbonka;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addIncome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_income);
+    }
+}
